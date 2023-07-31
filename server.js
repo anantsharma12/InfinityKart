0d360a94175108e06b97ad100928d6a9559b9e76
anantsharma12 <sharmaanant08@gmail.com>
2023-07-31 20:13:28 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,45 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import path from "path";
+
+//configure env
+dotenv.config();
+
+//database config
+connectDB();
+
+// rest object
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+//rest api
+app.get("*", function (req, res) {
+  res.sendFile(__dirname, "./client/build/index.html");
+});
+
+//PORT
+const PORT = process.env.port;
+
+//run listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Running in ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white
+  );
+});
