c1303f3a4e4cf4e90f088575e680d30ea65bdd90
anantsharma12 <sharmaanant08@gmail.com>
2023-08-01 02:36:57 +0530

    Second Commit

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import cors from "cors";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import path from "path";
+import { fileURLToPath } from "url";
 
 //configure env
 dotenv.config();
@@ -23,15 +24,19 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
-app.use(express.static(path.join(__dirname, "./client/build")));
+//app.use(express.static(path.join(__dirname, "./client/build")));
 
 //rest api
-app.get("*", function (req, res) {
-  res.sendFile(__dirname, "./client/build/index.html");
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to InfinityKart");
 });
 
 //PORT
